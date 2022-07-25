44629924ad8dbce44f4867b5f6adb22f635ea15f
Dani <dani@Danis-MacBook-Pro.local>
2022-07-25 08:40:27 +0800

    Updated folder structure

---
+++ src/headers.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+#include<cmath>
+#include<fstream>
+#include<sstream>
+#include<string>
--- src/main_rsa.cpp
+++ src/main_rsa.cpp
@@ -1,11 +1,4 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<vector>
-#include<cmath>
-#include<fstream>
-#include<sstream>
-#include<string>
+#include "headers.h"
 
 using namespace std; 
 
